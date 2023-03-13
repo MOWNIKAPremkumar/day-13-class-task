e286d9a6214105eec3a720380b710059fa8b2d72
N  Mownika <122893344+MOWNIKAPremkumar@users.noreply.github.com>
2023-03-14 01:38:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+    var a=document.getElementById("FName").value;
+    console.log(a);
+    var ab=document.getElementById("MName").value;
+    console.log(ab);
+    var abc=document.getElementById("LName").value;
+    console.log(abc);
+    var abcd=document.getElementById("e").value;
+    console.log(abcd);
+    var abcde=document.getElementById("pass").value;
+    console.log(abcde);
+    }
+    
